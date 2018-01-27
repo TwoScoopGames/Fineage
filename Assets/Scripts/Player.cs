2c57c16ea03cd35a1523cd6e8fd15c7431dbd20f
Eric Lathrop <eric@ericlathrop.com>
2018-01-27 14:08:54 -0500

    Move the square

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+  private Rigidbody2D rb;
+
+  void Start () {
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+  public float thrust = 1f;
+  public ForceMode2D mode = ForceMode2D.Force;
+
+  void Update () {
+    var horizontal = Input.GetAxis("Horizontal");
+    var vertical = Input.GetAxis("Vertical");
+    if (horizontal < 0) {
+      rb.AddForce(Vector2.left * thrust, mode);
+    }
+    if (horizontal > 0) {
+      rb.AddForce(Vector2.right * thrust, mode);
+    }
+    if (vertical < 0) {
+      rb.AddForce(Vector2.down * thrust, mode);
+    }
+    if (vertical > 0) {
+      rb.AddForce(Vector2.up * thrust, mode);
+    }
+  }
+}
