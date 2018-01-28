cd736bf40ac224ad9e3fdc932aaa9712f670797d
Eric Lathrop <eric@ericlathrop.com>
2018-01-28 14:07:59 -0500

    Add lung regen behaviour

--- Assets/Scripts/StaminaLungRegen.cs
+++ Assets/Scripts/StaminaLungRegen.cs
@@ -2,19 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StaminaMovementRegen : MonoBehaviour {
+public class StaminaLungRegen : MonoBehaviour {
 
   private Player player;
-  private Rigidbody2D rb;
 
   public int maxStamina = 1;
   public float staminaRegenPerSecond = 1f;
-  public float minRegenPercent = 0.1f;
 
   void Start() {
     var go = GameObjectExtensions.FindParentByTag(gameObject, "Player");
     player = go.GetComponent<Player>();
-    rb = go.GetComponent<Rigidbody2D>();
 
     player.maxStamina = maxStamina;
     player.stamina = maxStamina;
@@ -22,8 +19,8 @@ public class StaminaMovementRegen : MonoBehaviour {
 
   void Update() {
     var maxRegen = Time.deltaTime * staminaRegenPerSecond;
-    var minRegen = maxRegen * minRegenPercent;
-    var pct = rb.velocity.magnitude / 2000;
+    var minRegen = 0;
+    var pct = player.gameObject.transform.position.y > -20f ? 1f : 0f;
     var amt = Mathf.Lerp(minRegen, maxRegen, pct);
     player.stamina = Mathf.Min(maxStamina, player.stamina + amt);
   }
